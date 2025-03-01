446b187440db8fba8605d1a646ca9fda8e43bf3e
melvin <melvingerardo.04@gmail.com>
2025-03-01 17:25:34 +0800

    Add Restriction

--- resources/js/Pages/IPAddress/IPAddress.vue
+++ resources/js/Pages/IPAddress/IPAddress.vue
@@ -56,7 +56,7 @@ const deleteIPAddress = async (id) => {
                                     <td class="px-6 py-4 whitespace-nowrap">{{ ipAddress.name }}</td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <Link v-if="props.auth.user.id == ipAddress.user_id" :href="`/ipAddress/${ipAddress.id}/edit`" class="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 active:bg-green-700">Edit</Link>
-                                        <DangerButton   @click="deleteIPAddress(ipAddress.id)" class="text-red-600 hover:text-red-900 ml-4">Delete</DangerButton>
+                                        <DangerButton v-if="props.auth.user.user_type  == 'admin' "   @click="deleteIPAddress(ipAddress.id)" class="text-red-600 hover:text-red-900 ml-4">Delete</DangerButton>
                                     </td>
                                 </tr>
                             </tbody>
